78908597b8d8655c209530cc6a637428a3820f2a
Stefan Generalao <stegen@on.sinch.com>
2022-05-19 09:24:24 +0200

    Thing are happening

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import * as core from '@actions/core';
+import { listContacts, sendMessage } from './convesationAPI';
+import { fetchAccessToken } from './fetchAccessToken';
 
-const clientId = core.getInput('clientId');
-const clientSecret = core.getInput('clientSecret');
+(async () => {
+  const clientId = core.getInput('clientId');
+  const clientSecret = core.getInput('clientSecret');
+  const projectId = core.getInput('projectId');
+  const appId = core.getInput('appId');
 
-console.log('This MESSAGE is from Sinch Actions. Not the PoC:', {
-  clientId,
-  clientSecret,
-});
+  try {
+    const accessToken = await fetchAccessToken(clientId, clientSecret);
+
+    const contacts = await listContacts({
+      projectId,
+      accessToken,
+    });
+
+    await Promise.all(
+      contacts.map(async (contact: any) => {
+        const { id: contactId } = contact;
+
+        sendMessage({
+          accessToken,
+          message: {
+            text_message: { text: '💩💩💩💩💩💩💩🍌' },
+          },
+          contactId,
+          projectId,
+          appId,
+        });
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
+})();
